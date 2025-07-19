ce2d39e9f36460835d782dfd2f03855c81e23bd8
Shubhi Chourey <shubhi_chourey@epam.com>
2025-07-20 00:26:32 +0530

    pull merged

--- cloud-app/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login';
-import { SignupComponent } from './pages/signup/signup';
-
-export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'signup', component: SignupComponent },
-  { path: '**', redirectTo: 'login' },
-];
